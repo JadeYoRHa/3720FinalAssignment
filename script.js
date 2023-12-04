ad9ccc64d103b7ba2579b018fc4928cc579cbb03
JadeYoRHa <jade.macmaster@uleth.ca>
2023-12-03 19:50:30 -0800

    Done for the night

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Drag and Drop Functionality
 let dropZone = document.getElementById('drop_zone');
 
 dropZone.ondragover = function (event) {
@@ -28,21 +29,31 @@ dropZone.ondrop = function (event) {
 		sphere.style.top = `${y}px`;
 		sphere.style.width = '50px';
 		sphere.style.height = '50px';
-		sphere.style.backgroundColor = 'white';
 		sphere.style.borderRadius = '50%';
 		this.appendChild(sphere);
+
+		// Try to read the item as a file
+		let reader = new FileReader();
+		reader.onload = function (e) {
+			// If it succeeds, it's a file, so set the sphere's color to white
+			sphere.style.backgroundColor = 'white';
+		};
+		reader.onerror = function (e) {
+			// If it fails, it's likely a folder, so set the sphere's color to yellow
+			sphere.style.backgroundColor = '#F8D775';
+		};
+		reader.readAsArrayBuffer(files[i]);
 
 		// Calculate the differences between the sphere's initial position and the center of the window
 		let dx = (window.innerWidth / 2 - sphere.offsetWidth / 2) - x;
 		let dy = (window.innerHeight / 2 - sphere.offsetHeight / 2) - y;
 
-		// Animate the sphere towards the center of the window, shrink it until it becomes invisible, and make it spin
+		// Animate the sphere towards the center of the window, shrink it until it becomes invisible, and make it stretch
 		gsap.to(sphere, {
 			x: `+=${dx}`,
 			y: `+=${dy}`,
 			scale: 0,
-			rotation: 360,
-			duration: 1
+			duration: 0.5,
 		});
 	}
 };
